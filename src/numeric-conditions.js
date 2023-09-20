b7a62d2bfb34be8d458b04806f87e5770f361972
lokismelc <elviscu90@gmail.com>
2023-09-20 16:33:42 +0100

    Elvis

--- src/numeric-conditions.js
+++ src/numeric-conditions.js
@@ -9,16 +9,36 @@ const ARRAY_TWO = ['Hello', 'Conditions', NUM_ONE] // eslint-disable-line no-unu
 // 1. Use conditional statements to set answerOne to false if ARRAY_ONE is not empty
 // or true if it is empty
 let answerOne
+if (ARRAY_ONE.length === 0) {
+  answerOne = true
+} else {
+  answerOne = false
+}
+console.log(answerOne)
 
 // 2. Use conditional statements to set answerTwo to false if ARRAY_TWO is not empty
 // or true if it is empty
 let answerTwo
-
+if (ARRAY_TWO.length === 0) {
+  answerTwo = true
+} else {
+  answerTwo = false
+}
+console.log(answerTwo)
 // 3. Use conditional statements to set answerThree to true if NUM_ONE is more than NUM_TWO
 let answerThree
-
+if (NUM_ONE > NUM_TWO) {
+  answerThree = true
+}
+console.log(answerThree)
 // 4. Use conditional statements to set answerFour to true if NUM_ONE or NUM_TWO are included in ARRAY_TWO
 let answerFour
+if (ARRAY_TWO.includes(NUM_ONE) || ARRAY_TWO.includes(NUM_TWO)) {
+  answerFour = true
+} else {
+  answerFour = false
+}
+console.log(answerFour)
 
 // Don't edit the code below this line
 module.exports = {
--- src/string-conditions.js
+++ src/string-conditions.js
@@ -2,45 +2,77 @@
 const STR_ONE = 'Hello' // eslint-disable-line no-unused-vars
 
 let answerOne
-
+if (STR_ONE === 'Hello') {
+  answerOne = true
+} else {
+  answerOne = false
+}
+console.log(answerOne)
 // 2. Use conditional statements to set answerTwo to true if STR_TWO is not 'Hello'
 const STR_TWO = 'Goodbye' // eslint-disable-line no-unused-vars
 
 let answerTwo
-
+if (STR_TWO !== 'Hello') {
+  answerTwo = true
+}
+console.log(answerTwo)
 // 3. Use conditional statements to set answerThree to true if STR_THREE is
 // longer than STR_FOUR
 const STR_THREE = 'Hello' // eslint-disable-line no-unused-vars
 const STR_FOUR = 'Good' // eslint-disable-line no-unused-vars
 
 let answerThree
-
+if (STR_THREE.length > STR_FOUR.length) {
+  answerThree = true
+} else {
+  answerThree = false
+}
+console.log(answerThree)
 // 4. Use conditional statements to set answerFour to true
 // if STR_FIVE starts and ends with the same character, regardless of case
 
 const STR_FIVE = 'Alexandra' // eslint-disable-line no-unused-vars
 
 let answerFour
-
+const firstChar = STR_FIVE.charAt(0).toLocaleLowerCase()
+const lastChar = STR_FIVE.charAt(STR_FIVE.length - 1).toLocaleLowerCase()
+if (firstChar === lastChar) {
+  answerFour = true
+} else {
+  answerFour = false
+}
+console.log(answerFour)
 // 5. Use conditional statements to set answerFive to true
 // if STR_SIX starts and ends with the same character, regardless of case
 
 const STR_SIX = 'Joanna' // eslint-disable-line no-unused-vars
 
 let answerFive
+if (STR_SIX.charAt(0) === STR_SIX.charAt(STR_SIX.length - 1)) {
+  answerFive = true
+} else {
+  answerFive = false
+}
 
 // 6. Use conditional statements to set answerSix to the middle character of STR_SEVEN
 // if STR_SEVEN has an odd number of characters
 const STR_SEVEN = 'Kayla' // eslint-disable-line no-unused-vars
 
 let answerSix
-
+const middleChar = STR_SEVEN[2]
+if (STR_SEVEN !== STR_SEVEN % 2) {
+  answerSix = middleChar
+}
+console.log(answerSix)
 // 7. Use conditional statements to set answerSeven to the middle two characters of
 // STR_EIGHT if STR_EIGHT has an even number of characters
 const STR_EIGHT = 'Alex' // eslint-disable-line no-unused-vars
 
 let answerSeven
-
+if (STR_EIGHT.length % 2 === 0) {
+  answerSeven = STR_EIGHT[1] + STR_EIGHT[2]
+}
+console.log(answerSeven)
 // 8. Set answerEight to the appropriate season based on what MONTH is set to
 //
 // For example, if MONTH is 'January', answerEight should be 'Winter'
@@ -53,10 +85,35 @@ let answerSeven
 // December to February: Winter
 //
 // Run the test after changing the value of MONTH to check you've covered every month correctly
-const MONTH = 'January'
+const MONTH = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'November',
+  'December'
+]
 
 let answerEight
-
+if (MONTH === 'December' || MONTH === 'January' || MONTH === 'Febrary') {
+  answerEight = 'Winter'
+} else if (MONTH === 'March' || MONTH === 'April' || MONTH === 'May') {
+  answerEight = 'Spring'
+} else if (MONTH === 'June' || MONTH === 'July' || MONTH === 'August') {
+  answerEight = 'Summer'
+} else if (
+  MONTH === 'September' ||
+  MONTH === 'October' ||
+  MONTH === 'November'
+) {
+  answerEight = 'Autumn'
+}
+console.log(answerEight)
 module.exports = {
   answerOne,
   answerTwo,
